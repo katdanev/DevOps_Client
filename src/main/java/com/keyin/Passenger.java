7f8d104b354ded3e5ed7e6d43f6a15bdc0ca4ed5
katdanev <138736957+katdanev@users.noreply.github.com>
2024-07-01 14:45:45 -0230

    Update Passenger.java

--- src/main/java/com/keyin/Passenger.java
+++ src/main/java/com/keyin/Passenger.java
@@ -1,5 +1,8 @@
 package com.keyin;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Passenger {
 
     private int id;
@@ -7,9 +10,22 @@ public class Passenger {
     private String lastName;
     private String phoneNumber;
     private City city;
+    private List<Aircraft> aircraftList;
+
+    // Constructors
+    public Passenger() {
+    }
+
+    public Passenger(int id, String firstName, String lastName, String phoneNumber, City city, List<Aircraft> aircraftList) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.aircraftList = aircraftList;
+    }
 
     // Getters and setters
-
     public int getId() {
         return id;
     }
@@ -48,5 +64,38 @@ public class Passenger {
 
     public void setCity(City city) {
         this.city = city;
+    }
+
+    public List<Aircraft> getAircraftList() {
+        return aircraftList;
+    }
+
+    public void setAircraftList(List<Aircraft> aircraftList) {
+        this.aircraftList = aircraftList;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", city=" + city +
+                ", aircraftList=" + aircraftList +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Passenger passenger = (Passenger) o;
+        return id == passenger.id && Objects.equals(firstName, passenger.firstName) && Objects.equals(lastName, passenger.lastName) && Objects.equals(phoneNumber, passenger.phoneNumber) && Objects.equals(city, passenger.city) && Objects.equals(aircraftList, passenger.aircraftList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, phoneNumber, city, aircraftList);
     }
 }
