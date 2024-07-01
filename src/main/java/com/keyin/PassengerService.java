b701c5cead640eecf4b033b68f9c0dff8c0850f7
katdanev <138736957+katdanev@users.noreply.github.com>
2024-07-01 14:46:11 -0230

    Update PassengerService.java

--- src/main/java/com/keyin/PassengerService.java
+++ src/main/java/com/keyin/PassengerService.java
@@ -1,5 +1,7 @@
 package com.keyin;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
@@ -11,21 +13,48 @@ public class PassengerService {
             URL url = new URL("http://localhost:8080/passenger/" + passengerId + "/aircraft");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-            conn.connect();
 
             int responseCode = conn.getResponseCode();
             if (responseCode != 200) {
-                throw new RuntimeException("HttpResponseCode: " + responseCode);
+                throw new RuntimeException("Failed : HTTP error code : " + responseCode);
             } else {
-                Scanner sc = new Scanner(url.openStream());
-                while (sc.hasNext()) {
-                    String inline = sc.nextLine();
-                    System.out.println(inline);
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
                 }
-                sc.close();
             }
+            conn.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-}
+
+    public void listAirportsForPassenger(int passengerId) {
+
+
+            try {
+                URL url = new URL("http://localhost:8080/passenger/" + passengerId + "/airports");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+
+                int responseCode = conn.getResponseCode();
+                if (responseCode != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : " + responseCode);
+                } else {
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            System.out.println(line);
+                        }
+                    }
+                }
+                conn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
